3cdd9d6527572c57e168a7924b3b3c4493490cbc
SRIDHARAN <jjsridharan@gmail.com>
2019-01-01 19:33:51 +0530

    Readme added

---
+++ Main.bat
@@ -0,0 +1,35 @@
+@echo off
+
+:: This script will run alarm sound every 20 minutes by default. Users can give their custom timings as well.
+
+:: Check if alarm tune exists
+set AlarmPath=%cd%/Alarm.mp3
+if NOT EXIST %AlarmPath% (
+	set AlarmPath=C:\Windows\media\Alarm01.wav
+	)
+:: Section User Input: Find the default choice by user
+
+set /a "SleepTime=20*60"
+set /p IsDefault=Proceed with default 20 minutes Interaval(y/n)?
+if /I "%IsDefault%"=="Y" GOTO Execute
+if /I "%IsDefault%"=="YES" GOTO Execute
+GOTO GetTime
+
+:: If user gave no as answer get the time from user and set the sleep time
+:GetTime
+set /p SleepTime=Enter the time in minutes :
+set /a SleepTime=SleepTime+0
+if NOT %SleepTime%==0 (
+	set /a SleepTime=SleepTime*60
+	)else (
+	echo Non Numeric characters found. Please enter a valid number 
+	GOTO GetTime
+	)
+
+GOTO Execute
+
+:: This script will continue to execute unless user closes the terminal
+:Execute
+TIMEOUT /T %SleepTime%
+Start C:\Users\Sridharan\Downloads\Alarm.mp3
+GOTO Execute
---
+++ README.md
@@ -0,0 +1,12 @@
+Please find more info about 20-20-20 rule from the below links.
+http://www.optometrytimes.com/editors-choice-opt/deconstructing-20-20-20-rule-digital-eye-strain
+https://opto.ca/health-library/the-20-20-20-rule
+https://www.healthline.com/health/eye-health/20-20-20-rule#definition 
+
+
+This script is designed in such a way to allow you to follow 20-20-20 rule.
+
+Just run this script when you are start working. 
+It will alert every 20 minutes. 
+Time is configurable meaning you can set the default time for yourself. (Ex: 15 minutes)
+When you are done just close this script or rerun at anytime you want. 
